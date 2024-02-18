46838515e72a4ff9a4240ee4dac32e5677764880
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-02-18 15:29:31 +0900

    제품 data.json 추가

--- src/Components/Main/MainView.tsx
+++ src/Components/Main/MainView.tsx
@@ -2,7 +2,10 @@ import MainProduct from '../Product/MainProduct';
 import styles from './MainView.module.css';
 import Slider from './Slider';
 
+import dummy from '../../data/data.json';
+
 export default function MainView() {
+  console.log(dummy);
   return (
     <div className={styles.main}>
       <Slider />
