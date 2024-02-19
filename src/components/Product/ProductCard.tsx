342c67a10f8c32ff8d77ed6d1999ad2d4d5a81be
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-02-19 17:08:11 +0900

    ProductCard 컴포넌트 분리

---
+++ src/components/Product/ProductCard.tsx
@@ -0,0 +1,40 @@
+import styles from './ProductCard.module.css';
+
+export default function ProductCard() {
+  return (
+    <>
+      <div className={styles.productCard}>
+        <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
+        <div className={styles.productInfo}>
+          <p className={styles.title}>aaaaaaaaaaa</p>
+          <p className={styles.subTitle}>dddddd</p>
+        </div>
+        <p className={styles.price}>1111111원</p>
+      </div>
+      <div className={styles.productCard}>
+        <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
+        <div className={styles.productInfo}>
+          <p className={styles.title}>aaaaaaaaaaa</p>
+          <p className={styles.subTitle}>dddddd</p>
+        </div>
+        <p className={styles.price}>1111111원</p>
+      </div>
+      <div className={styles.productCard}>
+        <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
+        <div className={styles.productInfo}>
+          <p className={styles.title}>aaaaaaaaaaa</p>
+          <p className={styles.subTitle}>dddddd</p>
+        </div>
+        <p className={styles.price}>1111111원</p>
+      </div>
+      <div className={styles.productCard}>
+        <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
+        <div className={styles.productInfo}>
+          <p className={styles.title}>aaaaaaaaaaa</p>
+          <p className={styles.subTitle}>dddddd</p>
+        </div>
+        <p className={styles.price}>1111111원</p>
+      </div>
+    </>
+  );
+}
