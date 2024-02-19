5c6232a31c084120cef75fadd94e8dd4b53ba9ad
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-02-19 17:29:13 +0900

    ProductCard 수정

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -3,38 +3,38 @@ import styles from './ProductCard.module.css';
 export default function ProductCard() {
   return (
     <>
-      <div className={styles.productCard}>
+      <a className={styles.productCard}>
         <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
         <div className={styles.productInfo}>
           <p className={styles.title}>aaaaaaaaaaa</p>
           <p className={styles.subTitle}>dddddd</p>
         </div>
         <p className={styles.price}>1111111원</p>
-      </div>
-      <div className={styles.productCard}>
+      </a>
+      <a className={styles.productCard}>
         <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
         <div className={styles.productInfo}>
           <p className={styles.title}>aaaaaaaaaaa</p>
           <p className={styles.subTitle}>dddddd</p>
         </div>
         <p className={styles.price}>1111111원</p>
-      </div>
-      <div className={styles.productCard}>
+      </a>
+      <a className={styles.productCard}>
         <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
         <div className={styles.productInfo}>
           <p className={styles.title}>aaaaaaaaaaa</p>
           <p className={styles.subTitle}>dddddd</p>
         </div>
         <p className={styles.price}>1111111원</p>
-      </div>
-      <div className={styles.productCard}>
+      </a>
+      <a className={styles.productCard}>
         <img className={styles.productImg} src="src/assets/shoes/soccer_shoes01.jpeg" alt="축구화이미지" />
         <div className={styles.productInfo}>
           <p className={styles.title}>aaaaaaaaaaa</p>
           <p className={styles.subTitle}>dddddd</p>
         </div>
         <p className={styles.price}>1111111원</p>
-      </div>
+      </a>
     </>
   );
 }
