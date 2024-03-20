7e658a05c3f3a3a6e4d4a697a4e788bb84de0a4c
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-03-21 02:41:04 +0900

    CSS 수정

--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -85,7 +85,7 @@ const ProductList = ({ title, limit, selectedCategory, selectedBrand }: Items): 
               <Link to={`/product/${product.id}`} className={styles.productCard} key={product.id}>
                 <img className={styles.productImg} key={product.id} src={product.img} alt="상품 이미지" />
                 <div className={styles.productInfo}>
-                  <p className={styles.title}>{product.title}</p>
+                  <p className={styles.cardTitle}>{product.title}</p>
                 </div>
                 <p className={styles.price}>{product.price} 원</p>
               </Link>
