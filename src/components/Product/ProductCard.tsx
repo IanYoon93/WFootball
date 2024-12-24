c42442538faa395a0f7fcc87e036351bda51d990
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-12-25 03:44:31 +0900

    모바일 이미지 엑박 에러 테스트

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -15,7 +15,7 @@ const ProductCard = ({ products, limit }: { products: Product[]; limit: number }
       {products.slice(offset, offset + limit).map((product: Product) => (
         <div className={styles.productCard} key={product.id}>
           <Link to={`/${product.id}`} className={styles.productCard} key={product.id}>
-            <img className={styles.productImg} key={product.id} src={product.img.replace(/^public\/|^src\//, '/')} alt="상품 이미지" />
+            <img className={styles.productImg} key={product.id} src={product.img.replace(/^public\/|^src\//, '/')} alt="상품 이미지" referrerPolicy="no-referrer" />
             <div className={styles.productInfo}>
               <p className={styles.title}>{product.title}</p>
             </div>
