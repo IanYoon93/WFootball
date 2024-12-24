8d58aeab51cbe44444c88ccb127e9d2bffb586c9
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-12-25 04:55:10 +0900

    모바일 이미지 엑박 에러 테스트

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -10,12 +10,21 @@ const ProductCard = ({ products, limit }: { products: Product[]; limit: number }
   console.log(setPage);
   const offset = (pages - 1) * limit;
 
+  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
   return (
     <>
       {products.slice(offset, offset + limit).map((product: Product) => (
         <div className={styles.productCard} key={product.id}>
           <Link to={`/${product.id}`} className={styles.productCard} key={product.id}>
-            <img className={styles.productImg} key={product.id} src={product.img.replace(/^public\/|^src\//, '/')} alt="상품 이미지" referrerPolicy="no-referrer" />
+            <img
+              className={styles.productImg}
+              key={product.id}
+              src={isMobile ? product.img.replace('src/', '/') : product.img.replace(/^public\/|^src\//, '/')}
+              alt="상품 이미지"
+              referrerPolicy="no-referrer"
+            />
+            {/* <img className={styles.productImg} key={product.id} src={product.img.replace(/^public\/|^src\//, '/')} alt="상품 이미지" referrerPolicy="no-referrer" /> */}
             <div className={styles.productInfo}>
               <p className={styles.title}>{product.title}</p>
             </div>
