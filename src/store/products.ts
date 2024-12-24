76fd508d9ed97950853f592c9b1620ddbc2ed3e1
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2024-12-25 02:40:26 +0900

    경로 수정

--- src/store/products.ts
+++ src/store/products.ts
@@ -1,6 +1,6 @@
 import { selector } from 'recoil';
 
-const productsURL = '/data/data.json?v=1';
+const productsURL = '/data/data.json';
 // export const productsURL = import.meta.env.VITE_W_FOOTBALL_API;
 console.log(productsURL);
 
